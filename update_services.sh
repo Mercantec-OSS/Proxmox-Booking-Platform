e952f3c707f06c4e29f5b1bf592b748fd34ad1a7
Viktor Viskov <carrergt@gmail.com>
2024-09-09 13:27:49 +0200

    Change update_script.sh

--- update_services.sh
+++ update_services.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/bash
 PROJECT_PATH=$(pwd)
 
-cd $PROJECT_PATH
 git pull
-cd $PROJECT_PATH/automatization/vmware-automation
-git pull
-cd $PROJECT_PATH/backend/vmware-backend
-git pull
-cd $PROJECT_PATH/frontend/vmware-frontend
-git pull
-
-cd $PROJECT_PATH
 docker compose down
 docker compose up -d --build
