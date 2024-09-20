76fb6740fa26666ad49c031b6b9bc0243573d63a
Viktor Viskov <carrergt@gmail.com>
2024-09-20 12:12:28 +0200

    Update get vm ip logick. Delete unused scripts

---
+++ automatization/app/DTOs/ResponseVmIpDTO.cs
@@ -0,0 +1,23 @@
+public class ResponseVmIpDTO
+{
+    [JsonPropertyName("mac_address")]
+    public string MacAddress { get; set; } = "";
+
+    [JsonPropertyName("ip")]
+    public IPInfo IP { get; set; } = new IPInfo();
+
+    public class IPInfo
+    {
+        [JsonPropertyName("ip_addresses")]
+        public List<IPAddressInfo> IPAddresses { get; set; } = new List<IPAddressInfo>();
+        public class IPAddressInfo
+        {
+            [JsonPropertyName("ip_address")]
+            public string IPAddress { get; set; } = "";
+
+            [JsonPropertyName("prefix_length")]
+            public int PrefixLength { get; set; } = 0;
+
+        }
+    }
+}
