c9f89b88ed25a4c77097c603acd30f39805940b3
Viktor Viskov <carrergt@gmail.com>
2024-09-24 13:47:32 +0200

    Update restore backup script

--- db/restore_backup.sh
+++ db/restore_backup.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <database_name> <backup_file>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <database_name> <backup_file> <db_port>"
     exit 1
 fi
 
 db_name=$1
 backup_file=$2
+port=$3
 
-if [ ! -f "$backup_file" ]; then
-  echo "File $backup_file not found!"
-  exit 1
-fi
+read -sp "Enter MySQL root password: " db_password
+echo
 
 echo "Checking if the database $db_name exists..."
-mysql -u root -p -e "DROP DATABASE IF EXISTS $db_name;"
+mysql -u root -p"$db_password" -P $port -e "DROP DATABASE IF EXISTS $db_name;"
 
 echo "Creating database $db_name..."
-mysql -u root -p -e "CREATE DATABASE $db_name;"
+mysql -u root -p"$db_password" -P $port -e "CREATE DATABASE $db_name;"
 
 echo "Importing backup from $backup_file into database $db_name..."
-mysql -u root -p $db_name < "$backup_file"
+mysql -u root -p"$db_password" -P $port $db_name < "$backup_file"
 
 echo "Process completed."
