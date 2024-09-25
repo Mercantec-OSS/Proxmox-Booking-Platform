384d5bcea7099ca8d0f870a2f2e9679c2831d37c
Viktor Viskov <carrergt@gmail.com>
2024-09-25 09:23:55 +0200

    Fix restore backup script

--- backup/restore_backup.sh
+++ backup/restore_backup.sh
@@ -8,13 +8,13 @@ then
     echo "Restore backup from file $BACKUP_FILE"
 
     echo "Checking if the database $DATABASE exists..."
-    mysql -u $USER -p"$PASSWORD" -P $PORT -e "DROP DATABASE IF EXISTS $DATABASE;"
+    mysql -h $HOST -P $PORT -u $USER -p"$PASSWORD" -e "DROP DATABASE IF EXISTS $DATABASE;"
 
     echo "Creating database $DATABASE..."
-    mysql -u $USER -p"$PASSWORD" -P $PORT -e "CREATE DATABASE $DATABASE;"
+    mysql -h $HOST -P $PORT -u $USER -p"$PASSWORD" -e "CREATE DATABASE $DATABASE;"
 
     echo "Importing backup from $backup_file into database $DATABASE..."
-    mysql -u $USER -p"$PASSWORD" -P $PORT $DATABASE < "$BACKUP_FILE"
+    mysql -h $HOST -P $PORT -u $USER -p"$PASSWORD" $DATABASE < "$BACKUP_FILE"
 
     rm -f "$BACKUP_FILE"
     echo "-------------------------------------"
