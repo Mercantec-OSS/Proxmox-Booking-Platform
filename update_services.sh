4654c8d5782ad231dc5e402fce93ec10b6c28007
Viktor Viskov <carrergt@gmail.com>
2025-02-19 11:40:38 +0100

    Update helper scripts

--- update_services.sh
+++ update_services.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/bash
-user=$(sudo whoami)
-if [ $user != "root" ]; then
-    echo "You need for root permissions"
-    exit 1
-fi
-
 git pull
-
-sudo rm -rf ./backend/build/*
-sudo rm -rf ./frontend/build/*
-
-docker compose restart
+docker compose up -d --build
